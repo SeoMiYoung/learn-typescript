4a2a2363f7a0bd99ea54fb6cadc84554d6ca1631
SeoMiYoung <seo20seo00@naver.com>
2023-05-26 16:58:18 +0900

    [섹션2-1] npm을 통해 tsc 설치해서 사용하기

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,4 +1,17 @@
 
+// 파일명: sample.js
+// @ts-check
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
 function sum(a,b) { // a와 b는 숫자여야 한다
   return a+b;
 }
+
+sum(10, '20'); // '20'에 빨간색 밑줄이 안그어짐
+
+
+
+
