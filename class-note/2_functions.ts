b5caf8909270c5a4e8ea752d698f623e2bf39dd7
SeoMiYoung <seo20seo00@naver.com>
2023-05-28 16:58:34 +0900

    [섹션3-4] 함수 스펙에 유연한 js와 달리, 엄격한 ts

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -1,10 +1,15 @@
 // 함수의 파라미터의 타입을 정의하는 방식
-function sum(a: number,b: number) {
-  return a+b;
-}
-sum(10,20);
+// function sum(a: number,b: number) {
+//   return a+b;
+// }
+// sum(10,20);
 
 // 함수의 반환값에도 타입을 정의할 수 있다
 function add(): number {
   return 10; // number타입 반환
 }
+
+function sum(a: number,b: number): number {
+  return a+b;
+}
+sum(10,20,30,40); // 2개의 인수(arguments)가 필요한데 4개를 넣었다고 경고 알람이 뜸 => ts
