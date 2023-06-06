baa1fc1237e1709eb91e5519ba18067027155ea7
SeoMiYoung <seo20seo00@naver.com>
2023-06-06 23:14:38 +0900

    [섹션7-2] 유니온 타입의 장점 (if문으로 type 구별하기)

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -11,10 +11,17 @@ logMessage(3);
 
 /* --------------------- Union Type을 쓰자(두개 이상의 타입 설정 가능) -------------------- */
 
-function logMessage(value: string | number) {
-  console.log(value);
+function logMessage(value: string | number ) {
+  if (typeof value === 'number') {
+    value.toLocaleString(); // 이러면 number에 대한 속성들을 사용하기 좋음
+  }
+  if (typeof value === 'string') {
+    value.toString(); // 이러면 number에 대한 속성들을 사용하기 좋음
+  }
+  // string과 number가 아닌 type에 대해서 에러를 발생시킬 수 있다!
+  throw new TypeError('value must be string or number');
 }
 
 logMessage('hello');
 logMessage(3);
-logMessage(true); // boolean type은 없음
+logMessage(true); // boolean type은 선택지에 없음 -> 에러 발생
