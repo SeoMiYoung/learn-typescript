ec0e3fd3ea092fcaca1963eac62ca5c490876043
SeoMiYoung <seo20seo00@naver.com>
2023-06-06 21:20:15 +0900

    [섹션6-3] 가급적이면 interface를 쓰는 걸 권장하는 이유?

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -26,3 +26,14 @@ type Todo = { id: string; title: string; done: boolean };
 function getToDo(todo: Todo) {
 
 };
+
+/* ------------------------------- 확장이 불가능한 타입 ------------------------------ */
+/* 
+  type과 interface의 가장 큰 차이점은,
+  타입의 확장 가능/불가능 여부입니다.
+
+  인터페이스는 타입의 확장이 가능하지만,
+  type 별칭은 확장이 불가능합니다.
+
+  따라서 가능한, type보다는 interface사용을 권장합니다.
+*/
